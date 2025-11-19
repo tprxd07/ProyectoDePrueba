b8faca11e5320c65118348cdef1dd1d28a335c00
TomasP <tomaspere07@gmail.com>
2025-11-19 14:08:27 +0100

    Nuevo proyecto de prueba

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
-
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
-}
+/*
+Creamos un proyecto en nuestro equipo
+Subimos el proyecto a GitHub
+2b. Si no existe README.md, lo creamos con el contenido:
+     # Proyecto de prueba
+Añadimos .idea y *.iml a .gitignore
+Hacemos commit y subimos los cambios.
+Creamos una rama desarrollo
+Cambiamos a la rama desarrollo
+Modificamos README.md
+Hacemos commit y push
+Cambiamos a la rama master (o main)
+Hacemos merge de la rama desarrollo en master (o main)
+ANTES DE HACER PUSH modificamos README en GitHub
+Hacemos push y resolvemos el conflicto
+Entregar URL del repositorio
+ */
